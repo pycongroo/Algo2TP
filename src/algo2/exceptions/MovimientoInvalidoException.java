f25b1bf70314c439fb7b438d81c69db0f70666c5
pycongroo <joserpg94@gmail.com>
2015-11-09 12:02:58 -0300

    reseteado project

--- src/algo2/exceptions/MovimientoInvalidoException.java
+++
@@ -1,9 +0,0 @@
-package algo2.exceptions;
-@SuppressWarnings("serial")
-public class MovimientoInvalidoException extends RuntimeException
-{
-	public MovimientoInvalidoException(String msg)
-	{
-		super(msg);
-	}
-}
